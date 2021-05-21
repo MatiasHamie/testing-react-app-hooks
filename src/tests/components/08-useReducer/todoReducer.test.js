9eebd483dc3aeb237e62ba9389ddeed724c607e2
MatiasHamie <mnhamie@gmail.com>
2021-05-21 20:26:34 -0300

    Pruebas basicas en componente que usa useState y prueba completa de un reducer (todoReducer)

---
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -0,0 +1,44 @@
+import "@testing-library/jest-dom";
+import { todoReducer } from "../../../components/08-useReducer/todoReducer";
+// datos fijos para pruebas
+import { initialTodos, nuevoTodo } from "../../fixtures/demoTodos";
+
+describe("Pruebas en todoReducer", () => {
+  test("Debe de retonar el state por defecto", () => {
+    const state = todoReducer(initialTodos, {});
+
+    expect(state).toEqual(initialTodos);
+  });
+
+  test("Debe agregar 1 TODO", () => {
+    const action = {
+      type: "add",
+      payload: nuevoTodo,
+    };
+
+    const state = todoReducer(initialTodos, action);
+
+    expect(state.length).toEqual(3);
+    expect(state).toEqual([...initialTodos, nuevoTodo]);
+  });
+
+  test("Debe borrar 1 TODO", () => {
+    const action = {
+      type: "delete",
+      payload: 1,
+    };
+
+    const state = todoReducer(initialTodos, action);
+    expect(state.find((todo) => todo.id === 1)).toBe(undefined);
+  });
+
+  test("Debe hacer el toggle de 1 TODO", () => {
+    const action = {
+      type: "toggle",
+      payload: 1,
+    };
+
+    const state = todoReducer(initialTodos, action);
+    expect(state.find((todo) => todo.id === 1).done).toBe(true);
+  });
+});
---
+++ src/tests/fixtures/demoTodos.js
@@ -0,0 +1,18 @@
+export const initialTodos = [
+  {
+    id: 1,
+    desc: "Aprender React",
+    done: false,
+  },
+  {
+    id: 2,
+    desc: "Aprender MongoDB",
+    done: false,
+  },
+];
+
+export const nuevoTodo = {
+  id: 3,
+  desc: "Aprender NodeJS",
+  done: false,
+};
